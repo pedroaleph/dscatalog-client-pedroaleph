1f51f9e4690aee3ad5c35f3fa9403b2beb47d86e
Pedro Aleph <pedro.aleph.allen@gmail.com>
2021-02-02 16:28:00 -0400

    Spring initializr

---
+++ backend/src/main/java/com/devsuperior/dscatalog_client/DscatalogClientApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogClientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscatalog_client/DscatalogClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
