a28d05745ac0f6ffd69bcdad1e0bd46058cfa77d
Pedro Aleph <pedro.aleph.allen@gmail.com>
2021-02-02 17:51:57 -0400

    ClientDTO dto

--- backend/src/main/java/com/devsuperior/dscatalog_client/dto/ClientDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog_client/dto/ClientDTO.java
@@ -1,9 +1,11 @@
-package com.devsuperior.dscatalog_client.entities;
+package com.devsuperior.dscatalog_client.dto;
 
 import java.io.Serializable;
 import java.time.Instant;
 
-public class Client implements Serializable {
+import com.devsuperior.dscatalog_client.entities.Client;
+
+public class ClientDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
@@ -13,17 +15,26 @@ public class Client implements Serializable {
     private Instant birthDate;
     private Integer children;
 
-    public Client() {
+    public ClientDTO() {
 
     }
 
-    public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+    public ClientDTO(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
         this.id = id;
         this.name = name;
         this.cpf = cpf;
         this.income = income;
         this.birthDate = birthDate;
         this.children = children;
+    }
+
+    public ClientDTO(Client entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.cpf = entity.getCpf();
+        this.income = entity.getIncome();
+        this.birthDate = entity.getBirthDate();
+        this.children = entity.getChildren();
     }
 
     public Long getId() {
@@ -72,30 +83,5 @@ public class Client implements Serializable {
 
     public void setChildren(Integer children) {
         this.children = children;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Client other = (Client) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
     }
 }
