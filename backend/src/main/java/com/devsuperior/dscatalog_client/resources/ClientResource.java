9f2a96f8f8cb5f53fecf3de16b14fb698545db09
Pedro Aleph <pedro.aleph.allen@gmail.com>
2021-02-03 01:14:41 -0400

    Pagination

--- backend/src/main/java/com/devsuperior/dscatalog_client/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog_client/resources/ClientResource.java
@@ -1,12 +1,14 @@
 package com.devsuperior.dscatalog_client.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import com.devsuperior.dscatalog_client.dto.ClientDTO;
 import com.devsuperior.dscatalog_client.services.ClientService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,9 +29,13 @@ public class ClientResource {
     private ClientService service;
 
     @GetMapping
-    public ResponseEntity<List<ClientDTO>> findAll() {
-        List<ClientDTO> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+    public ResponseEntity<Page<ClientDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        Page<ClientDTO> paged = service.findAllPaged(pageRequest);
+        return ResponseEntity.ok().body(paged);
     }
 
     @GetMapping(value = "/{id}")
